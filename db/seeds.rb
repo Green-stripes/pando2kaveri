566f2c02f88a54a4e02575aa7903cd49cf748fa6
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-23 12:34:18 +0100

    added CSV data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+filepath = "./app/assets/data"
+
+i = 0
+
+CSV.foreach(filepath, headers: :first_row, col_sep: "\t") do |row|
+  p row[1]
+  puts "#{i} done"
+
+  i = i+1
+end
+
+
+# array = []
+# CSV.foreach((filepath), headers: true, col_sep: " ") do |row|
+#     array << row[]
+# end
+# puts array
+
+# require 'csv'
+# require 'scanf' if (RUBY_VERSION >= '1.9.3')
+
+# FORMAT = '%15c %d %d %d'
+
+# data = <<EOF >> filepath
+# EOF
+
+# data.split("\n").each do |li|
+#   fields = li.scanf(FORMAT)
+#   puts [fields.first.strip, *fields[1 .. -1]].to_csv
+# end
