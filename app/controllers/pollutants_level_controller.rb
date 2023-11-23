09816e193ab650cb6203a854f909ed2ef189c769
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-23 14:18:08 +0100

    created a new seed file and ran it

--- app/controllers/pollutants_level_controller.rb
+++ app/controllers/pollutants_level_controller.rb
@@ -2,7 +2,7 @@ class PollutantsLevelController < ApplicationController
 
   def create
     pollutants_level = Pollutants_level.new
-    pollutants_level.save
+    pollutants_level.save!
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,10 @@ CSV.foreach(filepath, headers: :first_row, col_sep: "\t") do |row|
   room.save!
   p room
   p row[1]
-  parameter = Parameter.new(type: row[1])
+  parameter = Parameter.new(compound: row[1])
   parameter.save!
-  reading = Pollutants_level.new(room: room, parameter: parameter, time_stamp: row[0], level: row[2] )
-  reading.create
+  reading = PollutantsLevel.new(room: room, parameter: parameter, time_stamp: row[0], level: row[2] )
+  reading.save!
   p reading
   i = i+1
 end
