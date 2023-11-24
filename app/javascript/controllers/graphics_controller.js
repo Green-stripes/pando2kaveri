9ae1c1136a9f4a63cf6075f52a7023b725432ad4
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-24 14:52:32 +0100

    cleaned up the code. Chart.js chart still not working

--- app/javascript/controllers/graphics_controller.js
+++ app/javascript/controllers/graphics_controller.js
@@ -1,36 +1,26 @@
-
 import { Controller } from "@hotwired/stimulus";
 import { Chart } from "chart.js";
 
 export default class extends Controller {
-  static targets = ['myChart'];
+  static targets = ["myChart"];
 
   connect() {
-    const data = JSON.parse(this.myChartTarget.getAttribute('data-graphics-chart-data')); // Fetch data from data attribute
-    debugger
+    const data = JSON.parse(this.myChartTarget.getAttribute("data-graphics-chart-data"));
+
     new Chart(this.myChartTarget, {
-      type: 'line',
+      type: "line",
       data: {
         labels: data.labels,
-        datasets: [{
-          label: 'CO2 Levels',
-          data: data.data,
-          // backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          // borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1
-        }]
+        datasets: [
+          {
+            label: "CO2 Levels",
+            data: data.data,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+          },
+        ],
       },
-      options: {
-        scales: {
-          x: {
-              type: 'time',
-              time: {
-                  unit: 'millisecond'
-              }
-          }
-      }
-  }
-      }
-    );
+      });
   }
 }
