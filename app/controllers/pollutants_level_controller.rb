9edb2a0148e8ff46592954145af9538018cd050d
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-24 13:22:26 +0100

    trying to get chart.js to work

--- app/controllers/pollutants_level_controller.rb
+++ app/controllers/pollutants_level_controller.rb
@@ -7,13 +7,28 @@ class PollutantsLevelController < ApplicationController
 
   def index
     @pollutants_levels = PollutantsLevel.all
-    # @data = PollutantsLevel.pluck(:level, :time_stamp)
-    # @data = [
-    #   { name: 'January', data: 20 },
-    #   { name: 'February', data: 35 },
-      # Add more data points as needed
-    # ]
+    @times_co2 = []
+    @levels_co2 = []
+    @times_TMP = []
+    @levels_TMP = []
+    @times_VOCT = []
+    @levels_VOCT = []
+    @times_HUM = []
+    @levels_HUM = []
+    @pollutants_levels.each do |entry|
+      if entry.parameter.compound == "CO2"
+        @times_co2 << entry.time_stamp
+        @levels_co2 << entry.level
+      elsif entry.parameter.compound == "TMP"
+        @times_TMP << entry.time_stamp
+        @levels_TMP << entry.level
+      elsif entry.parameter.compound == "VOCT"
+        @times_VOCT << entry.time_stamp
+        @levels_VOCT << entry.level
+      else
+        @times_HUM << entry.time_stamp
+        @levels_HUM << entry.level
+      end
+    end
   end
-
-
 end
--- app/javascript/controllers/graphics_controller.js
+++ app/javascript/controllers/graphics_controller.js
@@ -1,51 +1,119 @@
-import { Controller } from "@hotwired/stimulus"
-// import Chart from 'chart.js';
-import { Chart, registerables } from "chart.js";
-Chart.register(...registerables);
+// import { Controller } from "@hotwired/stimulus"
+// import { Chart, registerables } from "chart.js";
+// Chart.register(...registerables);
 
-// Connects to data-controller="graphics"
+// // Connects to data-controller="graphics"
+// export default class extends Controller {
+//     static targets = ['myChart', 'time', 'reading'];
+
+//     canvasContext() {
+//         return this.myChartTarget.getContext('2d');
+//     }
+
+//     connect() {
+//       new Chart(this.canvasContext(), {
+//           type: 'line',
+//           data: {
+//               labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//               datasets: [{
+//                   label: '# of Votes',
+//                   // data: [12, 19, 3, 5, 2, 3],
+//                   backgroundColor: [
+//                       'rgba(255, 99, 132, 0.2)',
+//                       'rgba(54, 162, 235, 0.2)',
+//                       'rgba(255, 206, 86, 0.2)',
+//                       'rgba(75, 192, 192, 0.2)',
+//                       'rgba(153, 102, 255, 0.2)',
+//                       'rgba(255, 159, 64, 0.2)'
+//                   ],
+//                   borderColor: [
+//                       'rgba(255, 99, 132, 1)',
+//                       'rgba(54, 162, 235, 1)',
+//                       'rgba(255, 206, 86, 1)',
+//                       'rgba(75, 192, 192, 1)',
+//                       'rgba(153, 102, 255, 1)',
+//                       'rgba(255, 159, 64, 1)'
+//                   ],
+//                   borderWidth: 1
+//               }]
+//           },
+//           options: {
+//               scales: {
+//                   y: {
+//                       beginAtZero: true
+//                   }
+//               }
+//           }
+//       });
+//   }
+// }
+
+// import { Controller } from "@hotwired/stimulus";
+// import { Chart } from "chart.js";
+
+// export default class extends Controller {
+//   static targets = ['myChart', 'time', 'reading'];
+
+//   canvasContext() {
+//     return this.myChartTarget.getContext('2d');
+//   }
+
+//   connect() {
+//     const data = this.myChartTarget.dataset.chartData; // Fetch data from data attribute
+//     const parsedData = JSON.parse(data); // Parse JSON data
+// debugger;
+//     new Chart(this.canvasContext(), {
+//       type: 'line',
+//       data: {
+//         labels: JSON.parse(this.timeTarget), // Use the labels from the parsed data
+//         datasets: [{
+//           label: JSON.parse(this.timeTarget),
+//           data: JSON.parse(this.levelTarget), // Use the data from the parsed data
+//           // backgroundColor: parsedData.backgroundColor,
+//           borderColor: parsedData.borderColor,
+//           borderWidth: parsedData.borderWidth
+//         }]
+//       },
+
+      // options: {
+      //   scales: {
+      //     y: {
+      //       beginAtZero: true
+      //     }
+        // }
+      // }
+//     });
+//   }
+// }
+
+import { Controller } from "@hotwired/stimulus";
+import { Chart } from "chart.js";
+
 export default class extends Controller {
-    static targets = ['myChart'];
+  static targets = ['myChart'];
 
-    canvasContext() {
-        return this.myChartTarget.getContext('2d');
-    }
-
-    connect() {
-        new Chart(this.canvasContext(), {
-            type: 'line',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    }
-
+  connect() {
+    const data = JSON.parse(this.myChartTarget.getAttribute('data-graphics-chart-data')); // Fetch data from data attribute
+    debugger
+    new Chart(this.myChartTarget, {
+      type: 'line',
+      data: {
+        labels: data.labels,
+        datasets: [{
+          label: 'CO2 Levels',
+          data: data.data,
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
   }
+}
