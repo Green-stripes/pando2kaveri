04674bf8459b27ac6cad11e2615d6c7de9149a3e
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-25 16:38:33 +0100

    added room 3 chart to index page

--- app/javascript/controllers/graphics_controller.js
+++ app/javascript/controllers/graphics_controller.js
@@ -3,23 +3,26 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 export default class extends Controller {
-  static targets = ["myChart"];
+  static targets = ["myChart", "myChart1"];
 
   connect() {
     const data = JSON.parse(
       this.myChartTarget.getAttribute("data-graphics-chart-data")
+    );
+    const data1 = JSON.parse(
+      this.myChart1Target.getAttribute("data-graphics-chart1-data")
     ); // Fetch data from data attribute
 
     new Chart(this.myChartTarget, {
       type: "line",
       options: {
         plugins: {
-            title: {
-                display: true,
-                text: 'Room 4'
-            }
-        }
-    },
+          title: {
+            display: true,
+            text: "Room 4",
+          },
+        },
+      },
       data: {
         labels: data.labels,
         datasets: [
@@ -47,6 +50,51 @@ export default class extends Controller {
           {
             label: "HUM Levels",
             data: data.data_HUM,
+            backgroundColor: "rgba(255, 99, 255, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+
+    new Chart(this.myChart1Target, {
+      type: "line",
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "Room 1",
+          },
+        },
+      },
+      data: {
+        labels: data1.labels,
+        datasets: [
+          {
+            label: "CO2 Levels",
+            data: data1.data_CO2,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "TMP Levels",
+            data: data1.data_TMP,
+            backgroundColor: "rgba(255, 255, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "VOCT Levels",
+            data: data1.data_VOCT,
+            backgroundColor: "rgba(255, 99, 255, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "HUM Levels",
+            data: data1.data_HUM,
             backgroundColor: "rgba(255, 99, 255, 0.2)",
             borderColor: "rgba(255, 99, 132, 1)",
             borderWidth: 1,
