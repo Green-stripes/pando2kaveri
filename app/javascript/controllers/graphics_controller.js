165709b1b4e5a5b596c73dadcbb9d79e38ca9f36
Kaveri Bhattacharya <kavericb@hotmail.com>
2023-11-25 16:45:25 +0100

    added chart for room 1 in last commit and room  3 in this commit

--- app/javascript/controllers/graphics_controller.js
+++ app/javascript/controllers/graphics_controller.js
@@ -3,7 +3,7 @@ import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
 
 export default class extends Controller {
-  static targets = ["myChart", "myChart1"];
+  static targets = ["myChart", "myChart1", "myChart3"];
 
   connect() {
     const data = JSON.parse(
@@ -11,7 +11,10 @@ export default class extends Controller {
     );
     const data1 = JSON.parse(
       this.myChart1Target.getAttribute("data-graphics-chart1-data")
-    ); // Fetch data from data attribute
+    );
+    const data3 = JSON.parse(
+      this.myChart3Target.getAttribute("data-graphics-chart3-data")
+    );// Fetch data from data attribute
 
     new Chart(this.myChartTarget, {
       type: "line",
@@ -95,6 +98,50 @@ export default class extends Controller {
           {
             label: "HUM Levels",
             data: data1.data_HUM,
+            backgroundColor: "rgba(255, 99, 255, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+    new Chart(this.myChart3Target, {
+      type: "line",
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "Room 3",
+          },
+        },
+      },
+      data: {
+        labels: data3.labels,
+        datasets: [
+          {
+            label: "CO2 Levels",
+            data: data3.data_CO2,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "TMP Levels",
+            data: data3.data_TMP,
+            backgroundColor: "rgba(255, 255, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "VOCT Levels",
+            data: data3.data_VOCT,
+            backgroundColor: "rgba(255, 99, 255, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+          },
+          {
+            label: "HUM Levels",
+            data: data3.data_HUM,
             backgroundColor: "rgba(255, 99, 255, 0.2)",
             borderColor: "rgba(255, 99, 132, 1)",
             borderWidth: 1,
